e7aaf1697f8ddf5c9f4242a1315121e57562a1f1
JoeCP17 <at365@naver.com>
2022-03-21 17:29:51 +0900

    Initial commit

---
+++ src/Chepter1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Chepter1 {
+
+    static int max4(int a, int b, int c, int d) {
+        int max = a ;
+        if (b > max) max = b;
+        if (c > max) max = c;
+        if (d > max) max = d;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(max4(1,3,2,5));
+
+
+
+    }
+}
