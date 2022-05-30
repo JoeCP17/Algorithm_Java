472a1c2938340d37620538c05f2b76586ad11ef1
Joe <at365@naver.com>
2022-05-31 00:59:32 +0900

    릿코드 문제풀이

---
+++ src/leetcode/Q29/Divide_Two_Integers.java
@@ -0,0 +1,28 @@
+package leetcode.Q29;
+
+// dividend : 나눌 수
+// divisor : 나누는 수
+
+// 나눴을때 소수점 외, 정수만 나오게 하는 문제
+
+import java.util.Scanner;
+
+class Solution{
+    public int divide(int dividend, int divisor) {
+        if(dividend == Integer.MIN_VALUE && divisor == -1)
+            return Integer.MAX_VALUE;
+
+        int a = Math.abs(dividend);
+        int b = Math.abs(divisor);
+        int res = 0;
+
+        while (a - b >= 0) {
+            int x = 0;
+            while(a-(b<<1<<x) >= 0)
+                x++;
+            res += (1 << x);
+            a -= (b << x);
+        }
+        return (dividend > 0 == divisor > 0) ? res : -res;
+    }
+}
